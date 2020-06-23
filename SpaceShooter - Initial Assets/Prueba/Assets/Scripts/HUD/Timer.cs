f969e2d65393a1ed95b8823d3a894ef5b59a09c5
k0o <sb.knot@gmail.com>
2020-06-23 00:51:29 -0400

    timer

---
+++ SpaceShooter - Initial Assets/Prueba/Assets/Scripts/HUD/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private Text counter;
+    private float min, sec;
+
+    void Start()
+    {
+        counter = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        min = (int) (Time.time / 60f);
+        sec = (int) (Time.time % 60f);
+        if (min > 9)
+        {
+            counter.rectTransform.localPosition = new Vector3(161.71f, counter.rectTransform.localPosition.y, counter.rectTransform.localPosition.z);
+            counter.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 102.4f);
+        }
+        counter.text = $"{min:0}:{sec:00}";
+    }
+}
