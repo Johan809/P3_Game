918b07b34f666db014bc5080a6c9d1dc01d6daa8
Juanpi26 <imperionsama@gmail.com>
2020-06-25 20:05:10 -0400

    GlobosSpawner completados.
    Tarea de los GLOBOS completada.

---
+++ SpaceShooter - Initial Assets/Prueba/Assets/Scripts/BalloonScripts/BalloonScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonScript : MonoBehaviour
+{
+    public float speed = 1f;
+    private bool canMove = true;
+    public float bound_Y = 5f;
+
+    private Animator anim;
+    private AudioSource explosionSound;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        explosionSound = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        Move();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (canMove)
+        {
+            Vector3 temp = transform.position;
+            temp.y += speed * Time.deltaTime;
+            transform.position = temp;
+
+            if (temp.y > bound_Y)
+                gameObject.SetActive(false);
+        }
+    }
+
+}
---
+++ SpaceShooter - Initial Assets/Prueba/Assets/Scripts/BalloonScripts/BalloonSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonSpawner : MonoBehaviour
+{
+
+    public float min_X = -5.5f, max_X = 9.0f;
+    public GameObject[] balloon_Prefabs;
+    
+
+    public float timer = 2f;
+
+    void Start()
+    {
+        Invoke("SpawnBalloon", timer);
+    }
+
+    // Update is called once per frame
+    void SpawnBalloon()
+    {
+
+        float pos_X = Random.Range(min_X, max_X);
+        Vector3 temp = transform.position;
+        temp.x = pos_X;
+
+        if (Random.Range(0, 2)> 0)
+        {
+            Instantiate(balloon_Prefabs[Random.Range(0, balloon_Prefabs.Length)], temp, Quaternion.identity);
+        }
+
+        Invoke("SpawnBalloon", timer);
+
+    }
+}
