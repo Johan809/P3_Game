8f6ffe85b90bdb40916734197ae2bed6237ad3d6
Juanpi26 <imperionsama@gmail.com>
2020-06-30 16:09:18 -0400

    BlueSpawner
    Se hizo el punto 14 que permite que aparezan mas globos azules.

---
+++ SpaceShooter - Initial Assets/Prueba/Assets/Scripts/BalloonScripts/BalloonSpawner2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonSpawner2 : MonoBehaviour
+{
+    public float min_X = -6.3f, max_X = 5.5f;
+    public GameObject BluBalloon;
+
+
+    public float timer = 1f;
+
+    void Start()
+    {
+        Invoke("SpawnBalloon", timer);
+    }
+
+    // Update is called once per frame
+    void SpawnBalloon()
+    {
+
+        float pos_X = Random.Range(min_X, max_X);
+        Vector3 temp = transform.position;
+        temp.x = pos_X;
+
+        Instantiate(BluBalloon, temp, Quaternion.identity);
+
+        Invoke("SpawnBalloon", timer);
+
+    }
+}
