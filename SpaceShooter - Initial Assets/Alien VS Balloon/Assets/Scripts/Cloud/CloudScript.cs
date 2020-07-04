ad3cb54126434ad7c324dee4ddaae2b28960d81e
Karen <karenonaly@gmail.com>
2020-07-04 15:30:42 -0400

    Nube

---
+++ SpaceShooter - Initial Assets/Alien VS Balloon/Assets/Scripts/Cloud/CloudScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudScript : MonoBehaviour
+{
+    public float speed = 5f;
+    private bool canMove = true;
+    public float bound_X = 7.67f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (canMove)
+        {
+            Vector3 temp = transform.position;
+            temp.x += speed * Time.deltaTime;
+            transform.position = temp;
+
+            if (temp.x > bound_X)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ SpaceShooter - Initial Assets/Alien VS Balloon/Assets/Scripts/Cloud/CloudSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+
+    public float min_Y = 3.62f, max_Y = 4.11f;
+    public GameObject nubePrefab;
+    public float timer = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnNube", timer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SpawnNube()
+    {
+        float pos_Y = Random.Range(min_Y, max_Y);
+        Vector3 temp = transform.position;
+        temp.y = pos_Y;
+
+        Instantiate(nubePrefab, temp, Quaternion.Euler(0f, 0f, 0f));
+        Invoke("SpawnNube", timer);
+    }
+}
